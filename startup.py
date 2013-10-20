7cdb23abc33daa2b03bd2b17e8bcbfb7ac7c614d
shrekno <tiankk@gmail.com>
2013-10-21 00:43:24 +0800

    Mod startup.py

--- startup.py
+++ startup.py
@@ -13,24 +13,11 @@ import subprocess
 
 from proj import conf
 
-collect_file_name = 'collect.py'
-merge_file_name = 'merge.py'
-
-profile_dir = u'./proj/profile'
-command_dir = u'./proj/command'
-cs_conf_dir = u'./proj/client_secrets'
-cs_data_dir = u'./proj/client_secrets'
-output_dir = u'./proj/output'
-collect_output_dir = u'./proj/output/collect'
-merge_output_dir = u'./proj/output/merge'
-log_dir = u'./proj/log'
-collect_log_dir = u'./proj/log/collect'
-merge_log_dir = u'./proj/log/merge'
 
 def start_proc(command, log_file_path, proc_list):
-  if os.path.exists(log_file):
-    os.remove(log_file)
-  f = codecs.open(log_file, encoding='utf-8', mode='w')
+  if os.path.exists(log_file_path):
+    os.remove(log_file_path)
+  f = codecs.open(log_file_path, encoding='utf-8', mode='w')
   if sys.platform.find('win') == 0:
     proc_list.append({u'proc':subprocess.Popen(command, stderr=f, stdout=f), u'stop':False})
   elif sys.platform.find('linux') == 0:
@@ -58,7 +45,7 @@ def create_log_file_path(log_dir, file_name_without_suffix):
     os.makedirs(log_dir)
   return os.path.join(log_dir, u'log_' + file_name_without_suffix + u'.txt')
 
-def check_log(log_dir):
+def check_log(log_dir, profile_dir):
   for profile_file in os.listdir(profile_dir):
     if os.path.isfile(os.path.join(profile_dir, profile_file)) and profile_file.find(u'.txt') > 0:
       log_file = create_log_file_path(log_dir, profile_file[:profile_file.find('.')])
@@ -79,17 +66,33 @@ def check_log(log_dir):
         print log_file, u'Is Not Exists.'
         exit(1)
 
-if __name__ == '__main__':
+def main(argv):
+  collect_file_name = 'collect.py'
+  merge_file_name = 'merge.py'
+
+  current_dir = os.path.dirname(__file__)
+
+  profile_dir = os.path.join(current_dir, u'proj/profile')
+  command_dir = os.path.join(current_dir, u'proj/command')
+  cs_conf_dir = os.path.join(current_dir, u'proj/client_secrets')
+  cs_data_dir = os.path.join(current_dir, u'proj/client_secrets')
+  output_dir = os.path.join(current_dir, u'proj/output')
+  collect_output_dir = os.path.join(current_dir, u'proj/output/collect')
+  merge_output_dir = os.path.join(current_dir, u'proj/output/merge')
+  log_dir = os.path.join(current_dir, u'proj/log')
+  collect_log_dir = os.path.join(current_dir, u'proj/log/collect')
+  merge_log_dir = os.path.join(current_dir, 'proj/log/merge')
+
   # the day before yesterday
   collect_date = datetime.date.today() - datetime.timedelta(days = 1)
 
-  if len(sys.argv) > 1:
+  if len(argv) > 1:
     argv_start_index = 1
     reg_date = r'[0-9]{4}-[0-9]{2}-[0-9]{2}'
-    if not re.match(reg_date, sys.argv[argv_start_index]):
+    if not re.match(reg_date, argv[argv_start_index]):
       print u'collect date error, reg:[0-9]{4}-[0-9]{2}-[0-9]{2}'
       exit(1) 
-    collect_date_array = sys.argv[argv_start_index].split('-')
+    collect_date_array = argv[argv_start_index].split('-')
     collect_date = datetime.date(int(collect_date_array[0]), int(collect_date_array[1]), int(collect_date_array[2]))
 
   # collect
@@ -97,7 +100,7 @@ if __name__ == '__main__':
     shutil.rmtree(os.path.join(collect_log_dir, str(collect_date)))
   collect_processes = []
   for profile_file in os.listdir(profile_dir):
-    if os.path.isfile(os.path.join(profile_dir, profile_file)):
+    if os.path.isfile(os.path.join(profile_dir, profile_file)) and profile_file.find(u'.txt') > 0:
       cs_file_name = u'cs_1'
       if profile_file in conf._cs_conf:
         cs_file_name = conf._cs_conf[profile_file]
@@ -120,7 +123,7 @@ if __name__ == '__main__':
 
   # verify
   print u'Check Collect Result Logs.'
-  check_log(os.path.join(collect_log_dir, str(collect_date)))
+  check_log(os.path.join(collect_log_dir, str(collect_date)), profile_dir)
   print u'All Collect Succ.'
 
   # merge
@@ -146,7 +149,7 @@ if __name__ == '__main__':
 
   # verify
   print u'Check Merge Result Logs.'
-  check_log(os.path.join(merge_log_dir, str(collect_date)))
+  check_log(os.path.join(merge_log_dir, str(collect_date)), profile_dir)
   print u'All Merge Succ.'
 
   # last merge
@@ -199,3 +202,7 @@ if __name__ == '__main__':
   print u'Last Merge Succ.'
 
   print u'All Succ.'
+
+
+if __name__ == '__main__':
+  main(sys.argv)
