09613bc117e4837cc77ff3609e4f9bdc9d1a0fe6
shrekno <tiankk@gmail.com>
2013-10-25 21:48:37 +0800

    Fix bug.

--- startup.py
+++ startup.py
@@ -58,7 +58,7 @@ def main(argv):
       if not os.path.exists(output_dir):
         os.makedirs(output_dir)
       print u'Start Collect', profile_file
-      collect.main(profile_file_path, cmd_file, cs_conf_file, cs_data_file, output_dir, str(collect_date), str(uuid.uuid4()))
+      collect.main([profile_file_path, cmd_file, cs_conf_file, cs_data_file, output_dir, str(collect_date), str(uuid.uuid4())])
   print u'All Collect Succ.'
 
   # merge
